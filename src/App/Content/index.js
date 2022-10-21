f20d936c6ea2c54ad44c9d3f5006ad4cda32511d
Wojciech Leksza <wojtek.leksza@gmail.com>
2022-10-21 18:09:21 +0200

    added options

--- src/App/Content/index.js
+++ src/App/Content/index.js
@@ -1,11 +1,13 @@
 import GameStatus from "./GameStatus";
 import GameWindow from "./GameWindow";
+import Settings from "./Settings";
 import { Container } from "./styled";
 
 const Content = () => (
     <Container>
         <GameStatus moves={12} time={"00:00:45"}/>
         <GameWindow />
+        <Settings />
     </Container>
 );
 
