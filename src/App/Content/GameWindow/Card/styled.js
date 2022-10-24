84ee3822566c8053447baad387a90ae05b7feb70
Wojciech Leksza <wojtek.leksza@gmail.com>
2022-10-24 14:59:13 +0200

    added styled component for image

--- src/App/Content/GameWindow/Card/styled.js
+++ src/App/Content/GameWindow/Card/styled.js
@@ -1,10 +1,16 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-    display: flex;
-    align-content: center;
-    justify-content: center;
+export const Container = styled.button`
+    aspect-ratio: 1;
+    border: ${props => (props.checked ? `solid 10px #000` : `none`)};
     border-radius: 5px;
-    background-color: #fff; // to remove
-    padding: 50px;
+    background-color: ${props => (props.matched ? `#888` : `#fff`)};
+
+    &:hover{
+        background-color: #eee;
+    }
+`;
+export const Picture = styled.img`
+    width: 60%;
+    margin: auto;
 `;
